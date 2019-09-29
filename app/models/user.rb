e8f6b9751b70c7f87b3d7169bc45817a48e9c133
goodjobbin85 <td1729@hotmail.com>
2019-09-29 15:04:55 +0000

    create remember method to create token for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     
-    def User.new_token
+    def self.new_token
         SecureRandom.urlsafe_base64
+    end
+    
+    def User.digest(string)
+        Digest::SHA1.hexdigest(string)
+    end
+    
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
     
     has_secure_password
